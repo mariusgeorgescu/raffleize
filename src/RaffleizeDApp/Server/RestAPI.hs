87470a4c3ae0acb9bf4746f8a6258d9d08ce4e0b
mariusgeorgescu <georgescumarius@live.com>
2024-11-19 01:17:05 +0200

    sse wip

--- src/RaffleizeDApp/Server/RestAPI.hs
+++ src/RaffleizeDApp/Server/RestAPI.hs
@@ -1,10 +1,14 @@
 module RestAPI where
 
+import Conduit
+import Control.Concurrent (threadDelay)
 import Control.Exception (try)
 import Control.Lens
 import Control.Monad.Reader
-import Control.Monad.Trans.Except (ExceptT (ExceptT))
+import Control.Monad.Trans.Except (ExceptT (ExceptT), handleE)
+import Data.Conduit.Combinators (concatMapM)
 import Data.Swagger
+import Data.Swagger.Internal.Schema (plain)
 import GeniusYield.Types
 import Network.HTTP.Types qualified as HttpTypes
 import Network.Wai.Middleware.Cors
@@ -13,24 +17,29 @@ import RaffleizeDApp.TxBuilding.Context
 import RaffleizeDApp.TxBuilding.Lookups
 import RaffleizeDApp.TxBuilding.Transactions
 import Servant
+import Servant.Conduit ()
 import Servant.Swagger
 
 type RaffleizeAPI =
   "build-tx" :> ReqBody '[JSON] RaffleizeInteraction :> Post '[JSON] String
-    :<|> "submit-tx" :> ReqBody '[JSON] AddWitAndSubmitParams :> Post '[JSON] ()
+    :<|> "submit-tx" :> ReqBody '[JSON] AddWitAndSubmitParams :> Post '[JSON] GYTxId
+    :<|> "submit-tx2" :> ReqBody '[JSON] AddWitAndSubmitParams :> StreamPost NewlineFraming JSON (ConduitT () () IO ())
     :<|> "raffles" :> Get '[JSON] [RaffleInfo]
     :<|> "user-raffles" :> ReqBody '[JSON] [GYAddress] :> Post '[JSON] [RaffleInfo]
     :<|> "user-raffles2" :> Capture "address" GYAddress :> Get '[JSON] [RaffleInfo]
     :<|> "user-tickets" :> Capture "address" GYAddress :> Get '[JSON] [TicketInfo]
+    :<|> "sse" :> StreamGet NewlineFraming JSON (ConduitT () Int IO ())
 
 raffleizeServer :: RaffleizeOffchainContext -> ServerT RaffleizeAPI IO
 raffleizeServer roc@RaffleizeOffchainContext {..} =
   handleInteraction roc
     :<|> handleSubmit providerCtx
+    :<|> handleSubmitAndAwait providerCtx
     :<|> handleGetRaffles providerCtx
     :<|> handleGetRafflesByAddresses providerCtx
     :<|> handleGetRafflesByAddress providerCtx
     :<|> handleGetMyTickets providerCtx
+    :<|> handdleSSE
 
 raffleizeApi :: Proxy RaffleizeAPI
 raffleizeApi = Proxy
@@ -73,7 +82,25 @@ handleInteraction roc i = do
   print i
   runReaderT (interactionToHexEncodedCBOR i) roc
 
-handleSubmit :: ProviderCtx -> AddWitAndSubmitParams -> IO ()
+handleSubmit :: ProviderCtx -> AddWitAndSubmitParams -> IO GYTxId
 handleSubmit providerCtx AddWitAndSubmitParams {..} = do
   let txBody = getTxBody awasTxUnsigned
-  void $ gySubmitTx (ctxProviders providerCtx) $ makeSignedTransaction awasTxWit txBody
+  gySubmitTx (ctxProviders providerCtx) $ makeSignedTransaction awasTxWit txBody
+
+handleSubmitAndAwait :: ProviderCtx -> AddWitAndSubmitParams -> IO (ConduitT () () IO ())
+handleSubmitAndAwait providerCtx params = do
+  let ctxProv = ctxProviders providerCtx
+  return $
+    yieldM (handleSubmit providerCtx params)
+      .| printC
+      .| mapMC (gyAwaitTxConfirmed ctxProv (GYAwaitTxParameters 30 10000000 1))
+      .| printC
+
+handdleSSE :: IO (ConduitT () Int IO ())
+handdleSSE = return $ yieldMany [1 :: Int .. 10000] .| mapMC (\i -> threadDelay 10000000 >> return i)
+
+-- TODO - FIX THIS
+instance ToSchema (ConduitT () Int IO ()) where
+  declareNamedSchema _ = plain $ sketchSchema @() ()
+instance ToSchema (ConduitT () () IO ()) where
+  declareNamedSchema _ = plain $ sketchSchema @() ()
